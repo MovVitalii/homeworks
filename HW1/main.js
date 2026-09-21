da4b6ba9996bc63c9598271c6ead7521d9291dd4
Vitalii <mvitaliy1995@gmail.com>
2026-09-21 17:48:12 +0200

    create folder homeworks and done hw1

---
+++ HW1/main.js
@@ -0,0 +1,43 @@
+let meet = 'hello';
+let name = 'owu';
+let domain = 'com';
+let country = 'ua';
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 3.14;
+let number5 = 2.7;
+let number6 = 16;
+let bool = true;
+let bool2 = false;
+console.log(meet);
+console.log(name);
+console.log(domain);
+console.log(country);
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+console.log(number6);
+console.log(bool);
+console.log(bool2);
+
+let firstName = 'Movchan';
+let middleName = 'Vitalii';
+let lastName = 'Volodymyrovych';
+let person = `${firstName} ${middleName}  ${lastName}`;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+let first_name = prompt('What is your first name?');
+let middle_name = prompt('What is your middle name?');
+let last_name = prompt('What is your last name?');
+console.log(`${first_name} ${middle_name}  ${last_name}`);
