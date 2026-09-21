2dd72fdb8e3b694138b743c2bbcce1051ade9770
Vitalii <mvitaliy1995@gmail.com>
2026-09-21 22:11:23 +0200

    hw2 is done

---
+++ HW2/main.js
@@ -0,0 +1,32 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log('Супер -', coursesAndDurationArray[0].title);
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log('Супер -', coursesAndDurationArray[1].title);
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log('Супер -', coursesAndDurationArray[2].title);
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log('Супер -', coursesAndDurationArray[3].title);
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log('Супер -', coursesAndDurationArray[4].title);
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log('Супер -', coursesAndDurationArray[5].title);
+}
+
+
+
+
+
