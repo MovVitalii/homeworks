68d72cc813c4f54fb7e494be58dedcbb18e2db83
Vitalii <mvitaliy1995@gmail.com>
2026-09-24 16:26:56 +0200

    hw5 is done

---
+++ HW5/main.js
@@ -0,0 +1,9 @@
+const exchange = (sumUAH, currencyValues, exchangeCurrency) => {
+    for (let item of currencyValues) {
+        if (item.currency === exchangeCurrency) {
+            return sumUAH / item.value;
+        }
+    }
+    console.log('Currency not found');
+};
+console.log(exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD'));
